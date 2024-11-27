3eaa4173edeeb50d0b2ff16f348922c719b8631d
FrannielArias <156683746+FrannielArias@users.noreply.github.com>
2024-11-26 22:00:53 -0400

    Cree el modelo de Habitaciones Detalle

---
+++ HotelSunset/Models/HabitacionDetalle.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelSunset.Models
+{
+    public class HabitacionDetalle
+    {
+        [Key]
+        public int HabitacionDetalleId { get; set; }
+
+        [ForeignKey("AgregadoId")]
+        public int AgregadoId { get; set; }
+
+        [ForeignKey("HabitacionId")]
+        public int HabitacionId { get; set; }
+        public Agregados? Agregados { get; set; }
+        public Habitaciones? Habitaciones { get; set; }
+        public int Cantidad { get; set; }
+
+        [Range(1, 4, ErrorMessage = "El monto debe ser mayor a 0.")]
+        public double Precio {  get; set; }
+    }
+}
