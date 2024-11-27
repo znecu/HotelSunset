38d65cff77d52f08493c4a35ac16b7fad0f5cb49
FrannielArias <156683746+FrannielArias@users.noreply.github.com>
2024-11-26 22:00:20 -0400

    Cree el modelo agregados

---
+++ HotelSunset/Models/Agregados.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelSunset.Models
+{
+    public class Agregados
+    {
+        [Key]
+        public int AgregadoId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio.")]
+        [RegularExpression("^[a-zA-Z0-9]+$", ErrorMessage = "Solo se permiten letras y numeros.")]
+        public string? Descripcion { get; set; }
+
+        public double Precio { get; set; }
+
+        public int Existencia { get; set; }
+    }
+}
