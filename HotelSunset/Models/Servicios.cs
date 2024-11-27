b3033aa1b3964758cbdb29c47c72549176618b03
FrannielArias <156683746+FrannielArias@users.noreply.github.com>
2024-11-26 22:02:03 -0400

    Cree el modelo de servicios

---
+++ HotelSunset/Models/Servicios.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelSunset.Models;
+
+public class Servicios
+{
+    [Key]
+    public int ServicioId { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "Solo se permiten letras .")]
+    public string? NombreServicio { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    [RegularExpression("^[a-zA-Z0-9]+$", ErrorMessage = "Solo se permiten letras y números.")]
+    public string? Descripcion { get; set; }
+}
