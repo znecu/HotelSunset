979cd62c1d420799756bee2e198e6dcd4db1bee6
znecu <alma_maria@ucne.edu.do>
2024-11-29 03:37:00 -0400

    Agregué expresiones regulares

--- HotelSunset/Models/Agregados.cs
+++ HotelSunset/Models/Agregados.cs
@@ -8,11 +8,14 @@ namespace HotelSunset.Models
         public int AgregadoId { get; set; }
 
         [Required(ErrorMessage = "Este campo es obligatorio.")]
-        [RegularExpression("^[a-zA-Z0-9]+$", ErrorMessage = "Solo se permiten letras y numeros.")]
+        [RegularExpression(@"^[a-zA-Z0-9\s]+$", ErrorMessage = "Solo se permiten letras y numeros.")]
         public string? Descripcion { get; set; }
 
+        [Required(ErrorMessage = "Este campo es obligatorio. ")]
+        [Range(1, double.MaxValue, ErrorMessage = "El precio es demasido alto para el sistema")]
         public double Precio { get; set; }
 
+        [Range(1, 500, ErrorMessage = "La capacidad debe ser entre 1 y 500.")]
         public int Existencia { get; set; }
     }
 }
