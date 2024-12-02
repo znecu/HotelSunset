7d4d191c793f3af1d6aa6a83729a0764d6062404
FrannielArias <156683746+FrannielArias@users.noreply.github.com>
2024-12-01 23:52:48 -0400

    Se relaciono el modelo Reserva con HabitacionReserva.

--- HotelSunset/Models/Reservas.cs
+++ HotelSunset/Models/Reservas.cs
@@ -7,17 +7,15 @@ public class Reservas
 {
     [Key]
     public int ReservaId { get; set; }
-    //[ForeignKey("ClienteId")]
-    //public int ClienteId { get; set; }
     public DateTime FechaInicio { get; set; }
     public DateTime FechaFinal { get; set; }
 
     [Required(ErrorMessage = "Este campo es obligatorio.")]
-    public bool? EstadoReserva { get; set; } = true;
 
     public double Total { get; set; }
 
     public Clientes? Clientes { get; set; }
-    public ICollection<Habitaciones>? habitaciones { get; set; } = new List<Habitaciones>();
+    
     public ICollection<ReservasDetalle> ReservasDetalles { get; set; } = new List<ReservasDetalle>();
+    public ICollection<HabitacionReserva> HabitacionReserva { get; set; } = new List<HabitacionReserva>();
 }
