59558e350242bd1fc87875c54b433e4bc7f322f4
znecu <alma_maria@ucne.edu.do>
2024-12-08 14:34:45 -0400

    Creé el OnModelCreating de metodo de pago

--- HotelSunset/Data/ApplicationDbContext.cs
+++ HotelSunset/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HotelSunset.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : IdentityDbContext<ApplicationUser>(options)
     {
         public DbSet<Agregados> Agregados { get; set; }
@@ -12,10 +12,31 @@ namespace HotelSunset.Data
         public DbSet<Clientes> Clientes { get; set; }
         public DbSet<HabitacionDetalle> HabitacionDetalle { get; set; }
         public DbSet<Habitaciones> Habitaciones { get; set; }
-        public DbSet<MetodoPago> MetodoPago { get; set; }
+        public DbSet<Tarjetas> Tarjetas { get; set; }
         public DbSet<Reservas> Reservas { get; set; }
-        public DbSet<ReservasDetalle> ReservasDetalle { get;set; }
+        public DbSet<ReservasDetalle> ReservasDetalle { get; set; }
         public DbSet<Servicios> Servicios { get; set; }
         public DbSet<TipoHabitaciones> TipoHabitaciones { get; set; }
+        public DbSet<MetodoPago> MetodoPago { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MetodoPago>().HasData(
+                new List<MetodoPago>()
+                {
+                    new()
+                    {
+                        MetodoPagoId = 1,
+                        Descripcion = "Débito"
+                    },
+
+                    new()
+                    {
+                        MetodoPagoId = 2,
+                        Descripcion = "Crédito"
+                    }
+                }
+            );
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
