7e46dd79676812aae0cfa1a33db52d3db2d91f3e
FrannielArias <156683746+FrannielArias@users.noreply.github.com>
2024-12-07 21:29:42 -0400

    .

--- HotelSunset/Models/Agregados.cs
+++ HotelSunset/Models/Agregados.cs
@@ -1,21 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace HotelSunset.Models
+namespace HotelSunset.Models;
+
+public class Agregados
 {
-    public class Agregados
-    {
-        [Key]
-        public int AgregadoId { get; set; }
+    [Key]
+    public int AgregadoId { get; set; }
 
-        [Required(ErrorMessage = "Este campo es obligatorio.")]
-        [RegularExpression(@"^[\p{L}\s]+$", ErrorMessage = "Solo se permiten letras, espacios y acentos.")]
-        public string? Descripcion { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    [RegularExpression(@"^[\p{L}\s]+$", ErrorMessage = "Solo se permiten letras, espacios y acentos.")]
+    public string? Descripcion { get; set; }
 
-        [Required(ErrorMessage = "Este campo es obligatorio. ")]
-        [Range(1, 500000, ErrorMessage = "El precio debe ser entre 1 y 500,000")]
-        public double Precio { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio. ")]
+    [Range(1, 500000, ErrorMessage = "El precio debe ser entre 1 y 500,000")]
+    public double Precio { get; set; }
 
-        [Range(1, 500, ErrorMessage = "La exitencia debe ser entre 1 y 500.")]
-        public int Existencia { get; set; }
-    }
+    [Range(1, 500, ErrorMessage = "La exitencia debe ser entre 1 y 500.")]
+    public int Existencia { get; set; }
 }
